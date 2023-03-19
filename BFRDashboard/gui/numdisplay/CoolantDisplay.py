60f2471f74632d13073523471ac887ac12a43975
Jason Liu <111265664+liug26@users.noreply.github.com>
2023-03-19 14:24:33 -0700

    Delete BFRDashboard directory

--- BFRDashboard/gui/numdisplay/CoolantDisplay.py
+++
@@ -1,7 +0,0 @@
-from gui.numdisplay.NumberDisplayWidget import NumberDisplayWidget
-
-
-class CoolantDisplay(NumberDisplayWidget):
-    def __init__(self, parent=None):
-        super().__init__("gui/resources/coolant.png", "F", False, parent)
-
